1f2dfaa432c620b96f5f7a682af7d82371aba87a
Leonardional <leonardional@gmail.com>
2022-10-10 21:36:51 +0700

    init

---
+++ application/logs/log-2022-10-10.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-10-10 02:32:21 --> Severity: 8192 --> idn_to_ascii(): INTL_IDNA_VARIANT_2003 is deprecated C:\wamp\www\bmn\system\libraries\Form_validation.php 1234
+ERROR - 2022-10-10 13:52:10 --> Severity: 8192 --> idn_to_ascii(): INTL_IDNA_VARIANT_2003 is deprecated C:\wamp\www\bmn\system\libraries\Form_validation.php 1234
+ERROR - 2022-10-10 13:52:21 --> Severity: 8192 --> idn_to_ascii(): INTL_IDNA_VARIANT_2003 is deprecated C:\wamp\www\bmn\system\libraries\Form_validation.php 1234
+ERROR - 2022-10-10 14:08:05 --> Severity: error --> Exception: syntax error, unexpected ':' C:\wamp\www\bmn\application\controllers\Items.php 611
+ERROR - 2022-10-10 14:30:57 --> Severity: Notice --> Undefined property: stdClass::$barcode C:\wamp\www\bmn\application\views\items\laporanlisttable.php 39
+ERROR - 2022-10-10 14:30:57 --> Severity: Notice --> Undefined property: stdClass::$barcode C:\wamp\www\bmn\application\views\items\laporanlisttable.php 39
+ERROR - 2022-10-10 14:31:01 --> Severity: Notice --> Undefined property: stdClass::$barcode C:\wamp\www\bmn\application\views\items\laporanlisttable.php 39
+ERROR - 2022-10-10 14:31:01 --> Severity: Notice --> Undefined property: stdClass::$barcode C:\wamp\www\bmn\application\views\items\laporanlisttable.php 39
+ERROR - 2022-10-10 14:31:07 --> Severity: Notice --> Undefined property: stdClass::$barcode C:\wamp\www\bmn\application\views\items\laporanlisttable.php 39
+ERROR - 2022-10-10 14:31:07 --> Severity: Notice --> Undefined property: stdClass::$barcode C:\wamp\www\bmn\application\views\items\laporanlisttable.php 39
+ERROR - 2022-10-10 14:31:07 --> Severity: Notice --> Undefined property: stdClass::$barcode C:\wamp\www\bmn\application\views\items\laporanlisttable.php 39
+ERROR - 2022-10-10 14:31:07 --> Severity: Notice --> Undefined property: stdClass::$barcode C:\wamp\www\bmn\application\views\items\laporanlisttable.php 39
